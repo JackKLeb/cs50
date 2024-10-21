2b77e8cd7413356eb7caa709b7a4a25e86f2ce9a
JackKLeb <Jack.e.kleb@gmail.com>
2024-10-21 18:02:25 +0000

    Mon, Oct 21, 2024, 2:02 PM -04:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,13 +1,6 @@
 answer = str(input("What is the answer to the Great Question of Life, the Universe and Everything? \n"))
-if answer == "42":
-    print("Yes")
-elif answer == "forty two":
-    print("Yes")
-elif answer == "forty-two":
-    print ("Yes")
-elif answer == "FoRty TwO":
-    print("Yes")
-elif answer == "42 ":
+right = ["42", "forty two", "forty-two"]
+if answer in right :
     print("Yes")
 else:
     print("No")
