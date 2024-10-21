585d80447b5f8cc995e1e34895f1251d687981b1
JackKLeb <Jack.e.kleb@gmail.com>
2024-10-21 18:04:44 +0000

    Mon, Oct 21, 2024, 2:04 PM -04:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,5 +1,5 @@
-answer = str(input("What is the answer to the Great Question of Life, the Universe and Everything? \n"))
-right = ["42", "forty two", "forty-two"]
+answer = input("What is the answer to the Great Question of Life, the Universe and Everything? \n").lower().strip()
+right = ["42", "forty two", "forty-two", " 42 "]
 if answer in right :
     print("Yes")
 else:
