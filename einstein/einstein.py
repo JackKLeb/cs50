7efa62b26aa6124b3cafa837cc357131d5e5727c
JackKLeb <Jack.e.kleb@gmail.com>
2024-10-21 13:49:40 +0000

    Mon, Oct 21, 2024, 9:49 AM -04:00

--- einstein/einstein.py
+++ einstein/einstein.py
@@ -1,8 +1,5 @@
-#m = int(input("What's your mass?\n"))
-#c = 300000000 ** 2
-#E = m * c
-#print(E)
+m = int(input("What's your mass?\n"))
+c = 300000000 ** 2
+E = m * c
+print(E)
 
-f = input("How much was the meal? ")
-f = dollars.replace("$", "")
-print(float(f))
