165b1920e8d4c1870ef6d95ad3ee639cc3c32c2c
JackKLeb <Jack.e.kleb@gmail.com>
2024-10-22 13:42:36 +0000

    Tue, Oct 22, 2024, 9:42 AM -04:00

--- slope/slope.py
+++ slope/slope.py
@@ -1,17 +1,21 @@
 def main():
+    #y = mx + b
     x1 = int(input("x1: "))
     y1 = int(input("y1: "))
     x2 = int(input("x2: "))
     y2 = int(input("y2: "))
-    y_intersept = int(input("y-intersept: "))
+    b = int(input("y-intersept: "))
 
     y = y2 - y1
     x = x2 - x1
-    slope = x / y
+    m = x / y
 
-    print("Slope:", slope)
+    print("Slope:", m)
 
-    
+    print("Your Line: y = ", m, "x + ", b, sep = "")
+
+
+
 
 
 
