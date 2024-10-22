5430222f117f405d8fe9c0aa3204ce56612608f7
JackKLeb <Jack.e.kleb@gmail.com>
2024-10-22 13:38:41 +0000

    Tue, Oct 22, 2024, 9:38 AM -04:00

---
+++ slope/slope.py
@@ -0,0 +1,19 @@
+def main():
+    x1 = int(input("x1: "))
+    y1 = int(input("y1: "))
+    x2 = int(input("x2: "))
+    y2 = int(input("y2: "))
+    y_intersept = int(input("y-intersept: "))
+
+    y = y2 - y1
+    x = x2 - x1
+    slope = x / y
+
+    print("Slope:", slope)
+
+    
+
+
+
+
+main()
