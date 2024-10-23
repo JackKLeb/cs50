364e3ebd74a08ca6a07a7ecf1a43110fba9b9369
JackKLeb <Jack.e.kleb@gmail.com>
2024-10-23 21:17:17 +0000

    Wed, Oct 23, 2024, 5:17 PM -04:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,4 +1,21 @@
+
+def main():
+
 In = input("Input: ")
 
 for l in In:
-    if l.isvowel()
+    if isvowel(l):
+        print("", end = "", sep = "")
+    else:
+        print(l, end = "", sep = "")
+
+
+
+def isvowel(letter):
+    if letter.lower() in ("a", "e", "i", "o", "u"):
+        return True
+    else:
+        return False
+
+
+main()
