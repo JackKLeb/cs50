e29228f1b413ce9100e719186146403fadd7f86e
JackKLeb <Jack.e.kleb@gmail.com>
2024-10-23 16:33:32 +0000

    Wed, Oct 23, 2024, 12:33 PM -04:00

--- Week2/coke/coke.py
+++ Week2/coke/coke.py
@@ -14,6 +14,11 @@ due = 50
 print("Amount due:", due)
 balance = input("Insert coins: ")
     while balance <= 50:
+        #ask for more coins
         add = input("Insert coins: ")
+        #add the amount given to the balance
         balance += add
+        #subtract the balance from the amount due
+        due -= balance
+
 
