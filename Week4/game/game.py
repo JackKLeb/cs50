2ce4889038a6d08b1e99f1440bcf9a4bbc062ff4
JackKLeb <Jack.e.kleb@gmail.com>
2024-12-13 07:27:42 -0500

    Create game.py

---
+++ Week4/game/game.py
@@ -0,0 +1,48 @@
+import random
+
+def main():
+    while True:
+        #check if number is int
+        try:
+            n = int(input("Level: "))
+            #check if number is positive
+            if n > 0:
+                break
+            else:
+                continue
+
+        except ValueError:
+            continue
+
+    x = random.randint(1, n)
+
+    #asks for a guess and outputs the respective answer
+    while True:
+        try:
+            g = int(input("Guess: "))
+            if g > 0:
+                c = check(g, x)
+                if len(c) == 11:
+                    print(c)
+                    break
+                else:
+                    print(c)
+                    continue
+            else:
+                continue
+
+        except ValueError:
+            continue
+
+def check(g, x):
+    if g == x:
+        a = "Just right!" #len 11
+        return a
+    elif g > x:
+        a = "Too large!" #len 10
+        return a
+    else:
+        a = "Too small!" #len 10
+        return a
+
+main()
